8666e948a3e656ddb02e47507fa169cd93c9d4fd
Hari Limaye <hari.limaye@arm.com>
2021-06-10 16:49:21 +0100

    Add minimal ethernet example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "mbed.h"
+#include "EthernetInterface.h"
 
 int main()
 {
+    // Initialize network interface:
+    EthernetInterface eth;
+
+    // Try to connect:
+    nsapi_error_t status = eth.connect();
+
+    if (status) {
+        printf("Could not configure ethernet - error code: %d\n", status);
+    } else {
+         printf("Interface configured successfully\n");
+    }
+
     while (true) {
-        printf("hello\n");
+       // do nothing now 
     }
 }
 
